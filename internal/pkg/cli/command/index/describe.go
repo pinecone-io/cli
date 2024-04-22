a5eb9b26845acd477f420272940b10ec4b64ca37
Jen Hamon <jhamon@pinecone.io>
2024-04-22 17:38:01 -0400

    Add table output to describe

--- internal/pkg/cli/command/index/describe.go
+++ internal/pkg/cli/command/index/describe.go
@@ -2,11 +2,17 @@ package index
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
 
-	"github.com/spf13/cobra"
-	text "github.com/pinecone-io/cli/internal/pkg/utils/text"
-	"github.com/pinecone-io/cli/internal/pkg/utils/exit"
 	"github.com/pinecone-io/cli/internal/pkg/utils/client"
+	"github.com/pinecone-io/cli/internal/pkg/utils/exit"
+	text "github.com/pinecone-io/cli/internal/pkg/utils/text"
+	"github.com/spf13/cobra"
+
+	"github.com/pinecone-io/go-pinecone/pinecone"
 )
 
 var describeHelpText = `A longer description that spans multiple lines and likely contains examples
@@ -18,6 +24,7 @@ to quickly create a Cobra application.`
 
 type DescribeCmdOptions struct {
 	name string
+	json bool
 }
 
 func NewDescribeCmd() *cobra.Command {
@@ -26,20 +33,62 @@ func NewDescribeCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "describe",
 		Short: "Get configuration and status information for an index",
-		Long: describeHelpText,
+		Long:  describeHelpText,
 		Run: func(cmd *cobra.Command, args []string) {
 			ctx := context.Background()
 			pc := client.NewPineconeClient()
 
-			idxs, err := pc.DescribeIndex(ctx, options.name)
+			idx, err := pc.DescribeIndex(ctx, options.name)
 			if err != nil {
 				exit.Error(err)
 			}
-			text.PrettyPrintJSON(idxs)
+
+			if options.json {
+				text.PrettyPrintJSON(idx)
+			} else {
+				printDescribeIndexTable(idx)
+			}
 		},
 	}
+
+	// required flags
 	cmd.Flags().StringVarP(&options.name, "name", "n", "", "name of index to describe")
 	cmd.MarkFlagRequired("name")
 
+	// optional flags
+	cmd.Flags().BoolVar(&options.json, "json", false, "output as JSON")
+
 	return cmd
+}
+
+func printDescribeIndexTable(idx *pinecone.Index) {
+	writer := tabwriter.NewWriter(os.Stdout, 12, 1, 4, ' ', 0)
+
+	columns := []string{"ATTRIBUTE", "VALUE"}
+	header := strings.Join(columns, "\t") + "\n"
+	fmt.Fprint(writer, header)
+
+	fmt.Fprintf(writer, "Name\t%s\n", idx.Name)
+	fmt.Fprintf(writer, "Dimension\t%d\n", idx.Dimension)
+	fmt.Fprintf(writer, "Metric\t%s\n", string(idx.Metric))
+	fmt.Fprintf(writer, "State\t%s\n", string(idx.Status.State))
+	fmt.Fprintf(writer, "Ready\t%t\n", idx.Status.Ready)
+	fmt.Fprintf(writer, "Host\t%s\n", idx.Host)
+
+	var specType string
+	if idx.Spec.Serverless == nil {
+		specType = "pod"
+		fmt.Fprintf(writer, "Spec\t%s\n", specType)
+		fmt.Fprintf(writer, "Cloud\t%s\n", "")
+		fmt.Fprintf(writer, "Region\t%s\n", "")
+		fmt.Fprintf(writer, "Environment\t%s\n", idx.Spec.Pod.Environment)
+	} else {
+		specType = "serverless"
+		fmt.Fprintf(writer, "Spec\t%s\n", specType)
+		fmt.Fprintf(writer, "Cloud\t%s\n", idx.Spec.Serverless.Cloud)
+		fmt.Fprintf(writer, "Region\t%s\n", idx.Spec.Serverless.Region)
+		fmt.Fprintf(writer, "Environment\t%s\n", "")
+	}
+
+	writer.Flush()
 }
--- internal/pkg/utils/client/client.go
+++ internal/pkg/utils/client/client.go
@@ -1,16 +1,13 @@
 package client
 
 import (
-	"fmt"
-	"os"
 	"github.com/pinecone-io/cli/internal/pkg/utils/exit"
 	"github.com/pinecone-io/go-pinecone/pinecone"
+	"os"
 )
 
 func NewPineconeClient() *pinecone.Client {
 	key := os.Getenv("PINECONE_API_KEY")
-	fmt.Println("called with key:", key)
-
 	pc, err := pinecone.NewClient(pinecone.NewClientParams{
 		ApiKey: key,
 	})
