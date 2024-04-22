73f50716fec5a82487443a716da29c905c739a29
Jen Hamon <jhamon@pinecone.io>
2024-04-21 21:11:29 -0400

    Fix typo

--- internal/pkg/utils/client/client.go
+++ internal/pkg/utils/client/client.go
@@ -9,7 +9,7 @@ import (
 
 func NewPineconeClient() *pinecone.Client {
 	key := os.Getenv("PINECONE_API_KEY")
-	fmt.Println("list called with key:", key)
+	fmt.Println("called with key:", key)
 
 	pc, err := pinecone.NewClient(pinecone.NewClientParams{
 		ApiKey: key,
