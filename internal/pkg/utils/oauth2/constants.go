d465b16ca4e2bd07496a72d5050de2343836db82
Jen Hamon <jhamon@pinecone.io>
2024-06-05 17:15:41 -0400

    Update OAuth client secret

--- internal/pkg/utils/oauth2/constants.go
+++ internal/pkg/utils/oauth2/constants.go
@@ -1,7 +1,7 @@
 package oauth2
 
 const (
-	Auth0ClientId = "XY4m3uRYoW6S0dK9ypXmM0Wc2bzUAdXW"
+	Auth0ClientId = "A4ONXSaOGstwwir0zUztoI6zjyt9zsRH"
 	Auth0URL      = "https://login.pinecone.io"
 	Auth0Audience = "https://us-central1-production-console.cloudfunctions.net/api/v1"
 )
