2f2775c088b07f0ca94d3242c5427175e3b68956
Austin DeNoble <austin.d@pinecone.io>
2024-06-09 16:51:33 -0400

    allow entering an interactive chat view through km chat without a message flag, update the PrintChatHistory function

--- internal/pkg/cli/command/km/chat.go
+++ internal/pkg/cli/command/km/chat.go
@@ -1,14 +1,22 @@
 package km
 
 import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
 	"github.com/pinecone-io/cli/internal/pkg/knowledge"
 	"github.com/pinecone-io/cli/internal/pkg/utils/configuration/state"
 	"github.com/pinecone-io/cli/internal/pkg/utils/exit"
 	"github.com/pinecone-io/cli/internal/pkg/utils/help"
+	"github.com/pinecone-io/cli/internal/pkg/utils/models"
 	"github.com/pinecone-io/cli/internal/pkg/utils/pcio"
 	"github.com/pinecone-io/cli/internal/pkg/utils/presenters"
 	"github.com/pinecone-io/cli/internal/pkg/utils/style"
-	"github.com/pinecone-io/cli/internal/pkg/utils/text"
 	"github.com/spf13/cobra"
 )
 
@@ -35,21 +43,13 @@ func NewKnowledgeModelChatCmd() *cobra.Command {
 				return
 			}
 
-			style.Spinner("", func() error {
-				resp, err := knowledge.GetKnowledgeModelSearchCompletions(options.kmName, options.message)
-				if err != nil {
-					exit.Error(err)
-				}
-
-				if options.json {
-					text.PrettyPrintJSON(resp)
-				} else {
-					for _, choice := range resp.Choices {
-						presenters.PrintKnowledgeChatResponse(choice.Message.Content)
-					}
-				}
-				return nil
-			})
+			// If no message is provided drop them into chat
+			if options.message == "" {
+				startChat(options.kmName)
+			} else {
+				// If message is provided, send it to the knowledge model
+				sendMessage(options.kmName, options.message)
+			}
 		},
 	}
 
@@ -62,4 +62,74 @@ func NewKnowledgeModelChatCmd() *cobra.Command {
 	cmd.AddCommand(NewKnowledgeModelChatDescribeCmd())
 
 	return cmd
+}
+
+func startChat(kmName string) {
+	reader := bufio.NewReader(os.Stdin)
+	pcio.Printf("Now chatting with knowledge model %s. Type your message and press Enter. Press CTRL+C to exit.\n\n", style.Emphasis(kmName))
+
+	// Display previous chat history
+	displayChatHistory(kmName)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+		pcio.Printf("\nExiting chat with knowledge model %s.\n\n", style.Emphasis(kmName))
+		os.Exit(0)
+	}()
+
+	for {
+		fmt.Print("> ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			pcio.Printf("Error reading input: %s\n", err)
+			continue
+		}
+
+		text = strings.TrimSpace(text)
+
+		if text != "" {
+			_, err := sendMessage(kmName, text)
+			if err != nil {
+				pcio.Printf("Error sending message: %s\n", err)
+				continue
+			}
+		}
+	}
+}
+
+func sendMessage(kmName string, message string) (*models.ChatCompletionModel, error) {
+	response := &models.ChatCompletionModel{}
+
+	err := style.Spinner("", func() error {
+		chatResponse, err := knowledge.GetKnowledgeModelSearchCompletions(kmName, message)
+		if err != nil {
+			exit.Error(err)
+		}
+
+		response = chatResponse
+
+		for _, choice := range chatResponse.Choices {
+			presenters.PrintKnowledgeChatResponse(choice.Message.Content)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func displayChatHistory(kmName string) {
+	chatHistory := state.ChatHist.Get()
+	chat, ok := (*chatHistory.History)[kmName]
+	if !ok {
+		pcio.Printf("No chat history found for knowledge model %s\n", style.Emphasis(kmName))
+		return
+	}
+
+	presenters.PrintChatHistory(chat)
 }
