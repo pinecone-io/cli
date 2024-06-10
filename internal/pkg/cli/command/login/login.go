77b0d69c2cb8dce0ec1328c08f5fbd8bd2d6585f
Jen Hamon <jhamon@pinecone.io>
2024-06-10 10:28:59 -0400

    After login, just target the first proj/org

--- internal/pkg/cli/command/login/login.go
+++ internal/pkg/cli/command/login/login.go
@@ -2,13 +2,7 @@ package login
 
 import (
 	"context"
-	"io"
-	"os"
-	"strings"
 
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/pinecone-io/cli/internal/pkg/dashboard"
 	"github.com/pinecone-io/cli/internal/pkg/utils/browser"
 	"github.com/pinecone-io/cli/internal/pkg/utils/configuration/secrets"
@@ -72,14 +66,21 @@ func NewLoginCmd() *cobra.Command {
 				return
 			}
 
-			// Ask the user to choose a target org
-			targetOrg := postLoginSetTargetOrg(orgsResponse)
+			targetOrg := orgsResponse.Organizations[0]
+			state.TargetOrg.Set(&state.TargetOrganization{
+				Name: targetOrg.Name,
+				Id:   targetOrg.Id,
+			})
 			pcio.Println()
-			pcio.Printf(style.SuccessMsg("Target org set to %s.\n"), style.Emphasis(targetOrg))
+			pcio.Printf(style.InfoMsg("Target org set to %s.\n"), style.Emphasis(targetOrg.Name))
 
-			// Ask the user to choose a target project
-			targetProject := postLoginSetupTargetProject(orgsResponse, targetOrg)
-			pcio.Printf(style.SuccessMsg("Target project set %s.\n"), style.Emphasis(targetProject))
+			targetProj := targetOrg.Projects[0]
+			state.TargetProj.Set(&state.TargetProject{
+				Name: targetProj.Name,
+				Id:   targetProj.Id,
+			})
+
+			pcio.Printf(style.InfoMsg("Target project set %s.\n"), style.Emphasis(targetProj.Name))
 
 			pcio.Println()
 			pcio.Println(style.CodeHint("Run %s to view or change the target context.", "pinecone target"))
@@ -90,221 +91,4 @@ func NewLoginCmd() *cobra.Command {
 	}
 
 	return cmd
-}
-
-func postLoginSetTargetOrg(orgsResponse *dashboard.OrganizationsResponse) string {
-	if len(orgsResponse.Organizations) < 1 {
-		log.Debug().Msg("No organizations found. Please create an organization before proceeding.")
-		exit.ErrorMsg("No organizations found. Please create an organization before proceeding.")
-	}
-
-	// var org dashboard.Organization
-	var orgName string
-	if len(orgsResponse.Organizations) == 1 {
-		orgName = orgsResponse.Organizations[0].Name
-		log.Info().Msgf("Only 1 org present so target org set to %s", orgName)
-	} else {
-		pcio.Println()
-		pcio.Println("Many API operations take place in the context of a specific org and project.")
-		pcio.Println(pcio.Sprintf("This CLI maintains a piece of state called the %s so it knows which \n", style.Emphasis("target")) +
-			"organization and project to use when calling the API on your behalf.")
-
-		orgNames := []string{}
-		for _, org := range orgsResponse.Organizations {
-			orgNames = append(orgNames, org.Name)
-		}
-
-		orgName = uiOrgSelector(orgNames)
-		for _, org := range orgsResponse.Organizations {
-			if org.Name == orgName {
-				state.TargetOrg.Set(&state.TargetOrganization{
-					Name: org.Name,
-					Id:   org.Id,
-				})
-				break
-			}
-		}
-	}
-	return orgName
-}
-
-func postLoginSetupTargetProject(orgs *dashboard.OrganizationsResponse, targetOrg string) string {
-	for _, org := range orgs.Organizations {
-		if org.Name == targetOrg {
-			if len(org.Projects) < 1 {
-				log.Debug().Msg("No projects found. Please create a project before proceeding.")
-				exit.ErrorMsg("No projects found. Please create a project before proceeding.")
-				return ""
-			} else if len(org.Projects) == 1 {
-				state.TargetProj.Set(&state.TargetProject{
-					Name: org.Projects[0].Name,
-					Id:   org.Projects[0].GlobalProject.Id,
-				})
-				return org.Projects[0].Name
-			} else {
-				projectItems := []string{}
-				for _, proj := range org.Projects {
-					projectItems = append(projectItems, proj.Name)
-				}
-				projectName := uiProjectSelector(projectItems)
-
-				for _, proj := range org.Projects {
-					if proj.Name == projectName {
-						state.TargetProj.Set(&state.TargetProject{
-							Name: proj.Name,
-							Id:   proj.GlobalProject.Id,
-						})
-						return proj.Name
-					}
-				}
-			}
-		}
-	}
-	return ""
-}
-
-func uiProjectSelector(projectItems []string) string {
-	var targetProject string = ""
-	m2 := NewList(projectItems, len(projectItems)+6, "Choose a project to target", func() {
-		pcio.Println("Exiting without targeting a project.")
-		pcio.Printf("You can always run %s to set or change a project context later.\n", style.Code("pinecone target"))
-		exit.Success()
-	}, func(choice string) string {
-		targetProject = choice
-		return "Target project: " + choice
-	})
-	if _, err := tea.NewProgram(m2).Run(); err != nil {
-		pcio.Println("Error running program:", err)
-		os.Exit(1)
-	}
-	return targetProject
-}
-
-func uiOrgSelector(orgNames []string) string {
-	var orgName string
-	m := NewList(orgNames, len(orgNames)+6, "Choose an organization to target", func() {
-		pcio.Println("Exiting without targeting an organization.")
-		pcio.Printf("You can always run %s to set or change a project context later.\n", style.Code("pinecone target"))
-		exit.Success()
-	}, func(choice string) string {
-		orgName = choice
-		return "Target organization: " + choice
-	})
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		pcio.Println("Error running program:", err)
-		os.Exit(1)
-	}
-	return orgName
-}
-
-type ListModel struct {
-	list     list.Model
-	choice   string
-	quitting bool
-	onQuit   func()
-	onChoice func(string) string
-}
-
-func (m ListModel) Init() tea.Cmd {
-	return nil
-}
-
-func (m ListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.list.SetWidth(msg.Width)
-		return m, nil
-
-	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "q", "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
-
-		case "enter":
-			i, ok := m.list.SelectedItem().(item)
-			if ok {
-				m.choice = string(i)
-			}
-			m.onChoice(m.choice)
-			return m, tea.Quit
-		}
-	}
-
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
-}
-
-func (m ListModel) View() string {
-	if m.choice != "" {
-		return "sounds great " + m.choice
-	}
-	if m.quitting {
-		m.onQuit()
-		return ""
-	}
-	return "\n" + m.list.View()
-}
-
-func mapStringsToItems(strings []string) []list.Item {
-	items := make([]list.Item, len(strings))
-	for i, s := range strings {
-		items[i] = item(s)
-	}
-	return items
-}
-
-func NewList(items []string, listHeight int, title string, onQuit func(), onChoice func(string) string) ListModel {
-	const defaultWidth = 20
-
-	l := list.New(mapStringsToItems(items), itemDelegate{}, defaultWidth, listHeight)
-	l.SetShowHelp(false)
-	l.Title = title
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-
-	return ListModel{
-		list:     l,
-		onQuit:   onQuit,
-		onChoice: onChoice,
-	}
-}
-
-const listHeight = 14
-
-var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(0)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(3)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("5"))
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-)
-
-type item string
-
-func (i item) FilterValue() string { return "" }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int                             { return 1 }
-func (d itemDelegate) Spacing() int                            { return 0 }
-func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
-	}
-
-	str := pcio.Sprintf("%d. %s", index+1, i)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	pcio.Fprint(w, fn(str))
 }
