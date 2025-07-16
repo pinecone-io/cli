51503715ed1eb5f287d334fc05a956afb08a5c76
Austin DeNoble <austin.d@pinecone.io>
2025-07-16 14:55:31 -0400

    update .goreleaser.yaml to rename built binaries to 'pc', and install this as the cli, also add a symlink to 'pinecone' in the brew install step so we can throw a warning about removal of 'pinecone'

--- cmd/pinecone/main.go
+++ cmd/pinecone/main.go
@@ -1,10 +1,21 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 Pinecone Systems, Inc.
 */
 package main
 
-import cliRootCmd "github.com/pinecone-io/cli/internal/pkg/cli/command/root"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	cliRootCmd "github.com/pinecone-io/cli/internal/pkg/cli/command/root"
+)
 
 func main() {
+	executableName := filepath.Base(os.Args[0])
+	if executableName == "pinecone" {
+		fmt.Fprintln(os.Stderr, "⚠️  Warning: The 'pinecone' command is deprecated. Please use 'pc' instead.")
+	}
+
 	cliRootCmd.Execute()
 }
