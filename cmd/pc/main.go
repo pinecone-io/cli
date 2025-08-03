510d63fafd7ce2940658bbc597128ea2e9168b3e
Austin DeNoble <austin.d@pinecone.io>
2025-08-03 13:47:53 -0400

    remove pinecone deprecation warning

--- cmd/pc/main.go
+++ cmd/pc/main.go
@@ -4,19 +4,9 @@ Copyright © 2025 Pinecone Systems, Inc.
 package main
 
 import (
-	"os"
-	"path/filepath"
-
 	cliRootCmd "github.com/pinecone-io/cli/internal/pkg/cli/command/root"
-	"github.com/pinecone-io/cli/internal/pkg/utils/pcio"
-	"github.com/pinecone-io/cli/internal/pkg/utils/style"
 )
 
 func main() {
-	executableName := filepath.Base(os.Args[0])
-	if executableName == "pinecone" {
-		pcio.Fprintf(os.Stderr, "⚠️  Warning: The '%s' command is deprecated. Please use '%s' instead.", style.Code("pinecone"), style.Code("pc"))
-	}
-
 	cliRootCmd.Execute()
 }
